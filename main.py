1be33974edc3a4e6b23f61dbaab4ba62ae448f91
Tiến Lý Thanh <career.tienlythanh@outlook.com>
2024-11-04 01:00:45 +0700

    Update filter transaction by credit logic

--- main.py
+++ main.py
@@ -18,12 +18,15 @@ def get_transactions(amount: str = "", message : str = "", page: int = 1):
 
     # Load min_offset: position of the first record have credit's value match with amount from user input
     # Load max_offset: position of the last record have credit's value match with amount from user input
-    try:
-        [min_offset, max_offset] = indexes[amount]
-    except:
+    if amount == "": 
         min_offset = 0
         max_offset = indexes['max_offset']
-
+    else:
+        try:
+            [min_offset, max_offset] = indexes[amount]
+        except:
+            # Amount can't be found in the index file -> No transaction match
+            return []
     # Filter records have the message from user input
     result = csvReader.filter_transaction_with_message('./sorted_transactions.csv', message, page, min_offset, max_offset)
 
