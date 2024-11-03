d9f2412ed043cdf65952bb06fb53eafe0f4b8a0d
Tiến Lý Thanh <career.tienlythanh@outlook.com>
2024-11-04 00:48:08 +0700

    Add comments

--- csv_reader.py
+++ csv_reader.py
@@ -13,29 +13,35 @@ class CSV_Reader(object):
             indexes = dict()
             prev_price = None
 
+            # Loop through each line, if the credit not exist in the indexes dict, add it to the dict
+            # Store offset as an array, first element is min_offset, second element is max_offset
             for line in file: 
-
                 decode_text = line.decode('utf-8')
-                price = decode_text.split(',')[2]
-                if prev_price != price and prev_price != None:
+                credit = decode_text.split(',')[2]
+
+                if prev_price != credit and prev_price != None:
                     indexes[prev_price].append(offset)
-                if indexes.get(price) is None: 
-                    indexes[price] = [offset]
-                prev_price = price
+                if indexes.get(credit) is None: 
+                    indexes[credit] = [offset]
+                prev_price = credit
                 offset += len(line)
 
         indexes[prev_price].append(offset)
-        indexes['max_offset'] = offset
+        indexes['max_offset'] = offset # offset of the last line in the csv file
 
         with open('index.json', 'w') as file: 
             json.dump(indexes, file, separators=(',', ':'))
 
     def filter_transaction_with_message(self, file_path, target_message, page, min_offset, max_offset):
         result = []
-        skip = -10 * (page - 1)
+        skip = -10 * (page - 1) # Split the result into each page, each page has 10 transactions
+
         with open(file_path, 'rb') as file: 
-            file.seek(min_offset)
+            # Jump to position of first transaction match by credit
+            file.seek(min_offset) 
             current_offset = file.tell()
+            # If current_offset > max_offset, 
+            # it means all transactions match by credit have been checked, stop now (don't need to read to the end of the file)
             while current_offset < max_offset: 
                 line = file.readline()
                 current_offset += len(line)
@@ -46,7 +52,9 @@ class CSV_Reader(object):
 
                 if skip >= 10:
                     return result 
+                # Check if detail of transaction match with the message
                 if re.search(target_message, message, re.IGNORECASE) is not None:
+                    # Only add transaction to the result if skip enough
                     if skip >= 0:
                         transaction = {
                             "date": line[0],
--- main.py
+++ main.py
@@ -15,12 +15,16 @@ def get_transactions(amount: str = "", message : str = "", page: int = 1):
         indexes = json.load(file)
 
     csvReader = CSV_Reader()
+
+    # Load min_offset: position of the first record have credit's value match with amount from user input
+    # Load max_offset: position of the last record have credit's value match with amount from user input
     try:
         [min_offset, max_offset] = indexes[amount]
     except:
         min_offset = 0
         max_offset = indexes['max_offset']
 
+    # Filter records have the message from user input
     result = csvReader.filter_transaction_with_message('./sorted_transactions.csv', message, page, min_offset, max_offset)
 
     return result
